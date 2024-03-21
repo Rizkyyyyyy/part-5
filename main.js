c18f183d67577b4501408db4a0b145be93e86f69
Rizkyyyyyy <91900194+Rizkyyyyyy@users.noreply.github.com>
2024-03-21 22:49:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+for (let z = 1; z <= 10; z++) {
+console.log('data z ke -', z)
+}
+
+let x = 1
+while (x <=10){
+    console.log('data x ke -', x)
+    x++
+}
+
+let y = 20
+do{
+    console.log('data y ke -', y)
+    y++
+}while (y <= 10)
